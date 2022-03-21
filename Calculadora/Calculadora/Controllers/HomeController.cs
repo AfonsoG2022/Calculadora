8a985d5b9d433e9905ab9d0b486f5508d8eddd2e
Afonso Graca <aluno20944@ipt.pt>
2022-03-21 15:31:19 +0000

    preparar control teste

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -4,22 +4,44 @@ using System.Diagnostics;
 
 namespace Calculadora.Controllers
 {
+    /*Herança da class controler*/
     public class HomeController : Controller
     {
+        /*Variavel interna*/
         private readonly ILogger<HomeController> _logger;
 
+        /*Construtor*/
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+
+        [HttpGet] //este anotador e facultativo
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+
+
+        /*CLASSE DE INTERFACE*/
+        /*Funcao resposanvel pela interacao do browser, metedo que recebe o pedido feito pelo browser*/
+       
+        //recebe os dados do servidor, valor do botao
+
+        [HttpPost] //SÓ qd o formulario for submetido em "psot" ele sera ecionado
+        public IActionResult Index( string botao)
         {
+
+
+            return View();
+        }
+
+
+
+
+        public IActionResult Privacy(){
             return View();
         }
 
