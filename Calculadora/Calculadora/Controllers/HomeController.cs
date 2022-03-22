70614842638d88da771f4eb32e29a07c99f7769c
Afonso Graca <aluno20944@ipt.pt>
2022-03-22 14:47:55 +0000

    adição de processamento

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -18,8 +18,11 @@ namespace Calculadora.Controllers
 
 
         [HttpGet] //este anotador e facultativo
-        public IActionResult Index()
-        {
+        public IActionResult Index(){
+
+            //preparar os dados a serem enviados para a View na primeira interação
+            ViewBag.Visor = "0";        //inicia a calculadora com 0
+
             return View();
         }
 
@@ -31,8 +34,59 @@ namespace Calculadora.Controllers
         //recebe os dados do servidor, valor do botao
 
         [HttpPost] //SÓ qd o formulario for submetido em "psot" ele sera ecionado
-        public IActionResult Index( string botao)
-        {
+        public IActionResult Index( string botao, string visor) {   //botao pode executar 18 acoes diferentes
+
+            //testar valor do 'botao'
+            switch (botao)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //pressionei um algarismo
+                    if(visor == "0") { visor = botao; }
+                    //tipo de dados do visor e string vai contactar na string
+                    else { visor = visor + botao; }
+                    //desafio: fazer em modo algebrico a operacao
+                    break;
+
+
+                case ",":
+                    //foi precionada a virgula
+                    //string com ''
+                    //virgula só uma vez
+                    if (!visor.Contains(',')) visor += ',';
+
+                    break;
+
+                case "+/-":
+
+                    //vamos inverter o valor do 'visor'
+                    //StartsWith, vamos usar para verificar se o numero e negatico, ou começa com o sinal de menos ou nao começa
+
+                    //Se o visor comecar com o sinal de menos
+                    if (visor.StartsWith('-')) visor = visor.Substring(1);
+                                                        //visor[1..];
+
+                    //retornar negativo
+                    else visor = '-' + visor;
+
+
+                    //sugestao: fazer de forma algebrica
+
+                    break;
+            }
+
+            //preparar dados para serem enviados à View
+            //Saco para a view, contentor que transporta dados do contoler para a view 
+            //dar a viewo valor do controller
+            ViewBag.Visor = visor;
 
 
             return View();
