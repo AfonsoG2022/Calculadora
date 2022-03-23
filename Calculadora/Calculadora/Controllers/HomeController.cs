67fc7d1af539ac73b440ac777381eb668ee1c226
Afonso Graca <aluno20944@ipt.pt>
2022-03-23 15:42:56 +0000

    finalizacao

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -10,6 +10,11 @@ namespace Calculadora.Controllers
         /*Variavel interna*/
         private readonly ILogger<HomeController> _logger;
 
+
+        //public string primeiroOperando;         //quando passa do controller para a view a variavel e destruida 
+
+
+
         /*Construtor*/
         public HomeController(ILogger<HomeController> logger)
         {
@@ -30,11 +35,17 @@ namespace Calculadora.Controllers
 
         /*CLASSE DE INTERFACE*/
         /*Funcao resposanvel pela interacao do browser, metedo que recebe o pedido feito pelo browser*/
-       
+
         //recebe os dados do servidor, valor do botao
 
         [HttpPost] //SÓ qd o formulario for submetido em "psot" ele sera ecionado
-        public IActionResult Index( string botao, string visor) {   //botao pode executar 18 acoes diferentes
+        public IActionResult Index(
+            string botao,
+            string visor,
+            string primeiroOperando,            //recupera no controller os dados
+            string Operador,                     //recupera os dados
+            string limpaEcra
+            ) {   //botao pode executar 18 acoes diferentes
 
             //testar valor do 'botao'
             switch (botao)
@@ -50,9 +61,11 @@ namespace Calculadora.Controllers
                 case "9":
                 case "0":
                     //pressionei um algarismo
-                    if(visor == "0") { visor = botao; }
+                    //    || = ou
+                    if (limpaEcra == "sim" || visor == "0") { visor = botao; }
                     //tipo de dados do visor e string vai contactar na string
                     else { visor = visor + botao; }
+                    limpaEcra = "não";
                     //desafio: fazer em modo algebrico a operacao
                     break;
 
@@ -72,16 +85,93 @@ namespace Calculadora.Controllers
 
                     //Se o visor comecar com o sinal de menos
                     if (visor.StartsWith('-')) visor = visor.Substring(1);
-                                                        //visor[1..];
+                    //visor[1..];
 
                     //retornar negativo
                     else visor = '-' + visor;
 
 
-                    //sugestao: fazer de forma algebrica
-
                     break;
-            }
+
+
+
+                case "+":
+                case "-":
+                case "x":
+                case ":":
+
+                    //foi pressionado um operador
+                    if (!string.IsNullOrEmpty(Operador))
+                        
+                    {   
+                        //como já é, pelo menos, a mesma 
+                        
+                        //se o operador nao e nulo nem vazio executa as contas
+
+
+                 
+                        //agora, temos mesmo de fazer a operacao algebrica
+                        double operandoUm = Convert.ToDouble(primeiroOperando.Replace(',','.'));  //converte os valores             //caso ouver , colocar .
+                        double operandoDois = Convert.ToDouble(visor.Replace(',','.'));
+
+                        double resultado = 0;
+
+
+
+                        switch (Operador)
+                        {      // Se o operador
+                            case "+":
+                                resultado = operandoUm + operandoDois;       // para o +
+                                break;
+                            case "-":
+                                resultado = operandoUm - operandoDois;       // para o -
+                                break;
+                            case "*":
+                                resultado = operandoUm * operandoDois;       // para o *
+                                break;
+                            case "/":
+                                resultado = operandoUm / operandoDois;       // para o /
+                                break;
+                        }
+
+
+
+
+                        visor = resultado + "";     //transforma tudo o que esta na adição 
+
+
+                    }
+
+
+
+                    primeiroOperando = visor;
+                    Operador = botao;
+                    limpaEcra = "sim";
+
+            break;
+
+
+
+
+
+        }
+
+
+
+
+            //preparar dados para serem enviados para a VIEW
+            ViewBag.Visor = visor;
+            ViewBag.PrimeiroOperando = primeiroOperando;
+            ViewBag.Operando = Operador;
+            ViewBag.limpaEcra = limpaEcra;
+
+            return View();
+
+
+
+        //}
+
+            
 
             //preparar dados para serem enviados à View
             //Saco para a view, contentor que transporta dados do contoler para a view 
